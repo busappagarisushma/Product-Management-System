97a8774a682d30ddff6acf3804cf60959b3c97e3
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-21 19:34:10 +0530

    creating persistence.xml

--- src/main/java/com/example/product/entity/Product.java
+++ src/main/java/com/example/product/entity/Product.java
@@ -1,5 +1,50 @@
 package com.example.product.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int productId;
+	private String title;
+	private String description;
+	private int quantity;
+	private double price;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
